06913c6f60ca68b1b8d35a862054c0e9f205db43
NikitinR <ramzes.nro@gmail.com>
2022-11-02 17:26:44 +0100

    control_panel - implemented game control

--- src/features/ControlPanel/ControlPanel.tsx
+++ src/features/ControlPanel/ControlPanel.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classNames from 'classnames';
 
 import { useAppDispatch, useAppSelector } from '../../redux/hooks';
-import { updateScore } from '../../redux/slices/scoreBoardSlice';
+import { startGame, updateScore, resetGame, renameHomeTeam, renameAwayTeam } from '../../redux/slices/scoreBoardSlice';
 
 import Button from '../../components/Elements/Button/Button';
 import Input from '../../components/Elements/Input/Input';
@@ -10,14 +10,34 @@ import Input from '../../components/Elements/Input/Input';
 import classes from './ControlPanel.module.scss';
 
 const ControlPanel = () => {
+  const [startedStatus, setStartedStatus] = useState(false);
+
   const scoreBoard = useAppSelector((state) => state.scoreBoard);
   const dispatch = useAppDispatch();
+
+  const renameTeamHandler = (e: React.ChangeEvent<HTMLInputElement>, actionType: string) => {
+    const teamName = e.target.value;
+
+    if (actionType === 'homeTeam') {
+      dispatch(renameHomeTeam(teamName));
+    } else {
+      dispatch(renameAwayTeam(teamName));
+    }
+  };
+
+  const startGameHandler = () => {
+    dispatch(startGame(Math.floor(Math.random() * 100)));
+    setStartedStatus(true);
+  };
 
   const updateScoreHandler = (score: Array<number>) => {
     dispatch(updateScore(score));
   };
 
-  const stubAction = () => console.log('action works');
+  const finishGameHandler = () => {
+    dispatch(resetGame());
+    setStartedStatus(false);
+  };
 
   return (
     <>
@@ -26,18 +46,18 @@ const ControlPanel = () => {
           <div className={classes.InputWrapper}>
             <Input
               label={'Home team'}
-              disabled={false}
+              disabled={startedStatus}
               inputValue={scoreBoard.homeTeam}
-              changeAction={stubAction}
+              changeAction={(e) => renameTeamHandler(e, 'homeTeam')}
               placeholder={'Enter home team title'}
             />
           </div>
           <div className={classes.InputWrapper}>
             <Input
               label={'Away team'}
-              disabled={false}
+              disabled={startedStatus}
               inputValue={scoreBoard.awayTeam}
-              changeAction={stubAction}
+              changeAction={(e) => renameTeamHandler(e, 'awayTeam')}
               placeholder={'Enter away team title'}
             />
           </div>
@@ -46,24 +66,28 @@ const ControlPanel = () => {
         <div className={classes.GameActions}>
           <div className={classNames(classes.ControlGroup, classes.StartEnd)}>
             <div className={classes.ButtonWrapper}>
-              <Button clickAction={stubAction} disabled={false} title='Start' />
+              <Button
+                clickAction={startGameHandler}
+                disabled={startedStatus || scoreBoard.homeTeam.length < 1 || scoreBoard.awayTeam.length < 1}
+                title='Start'
+              />
             </div>
             <div className={classes.ButtonWrapper}>
-              <Button clickAction={stubAction} disabled={false} title='Finish' />
+              <Button clickAction={finishGameHandler} disabled={!startedStatus} title='Finish' />
             </div>
           </div>
           <div className={classNames(classes.ControlGroup, classes.UpdateScore)}>
             <div className={classes.ButtonWrapper}>
               <Button
                 clickAction={() => updateScoreHandler([scoreBoard.score[0] + 1, scoreBoard.score[1]])}
-                disabled={false}
+                disabled={!startedStatus}
                 title='Home team + 1'
               />
             </div>
             <div className={classes.ButtonWrapper}>
               <Button
                 clickAction={() => updateScoreHandler([scoreBoard.score[0], scoreBoard.score[1] + 1])}
-                disabled={false}
+                disabled={!startedStatus}
                 title='Away team + 1'
               />
             </div>
